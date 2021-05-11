90f0f21242b9a5b49b1e99766be0600a9ffd99eb
Antin Williams <williamsantin43@gmail.com>
2021-05-11 19:10:08 +0200

    Finished up ListAppTest code

--- src/test/java/za/ac/cput/ListAppTest.java
+++ src/test/java/za/ac/cput/ListAppTest.java
@@ -1,12 +1,34 @@
 package za.ac.cput;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ListAppTest {
-
+    List list;
     @BeforeEach
     void setUp() {
+        list = new LinkedList();
+        list.add(0, "Apples");
+        list.add(1, "Bananas");
+        list.add(2, "Oranges");
+    }
+    @Test
+    public void testListAdd(){
+        list.add(3, "Peaches");
+        assertTrue(list.contains("Peaches"));
+    }
+    @Test
+    public void testListRemove(){
+        list.remove("Apples");
+        assertFalse(list.contains("Apples"));
+    }
+    @Test
+    public void testListFind(){
+        assertTrue(list.contains("Bananas"));
     }
 }
