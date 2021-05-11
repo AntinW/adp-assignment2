bfc611e03e7fd818198d23f371fbaf702ea8be3f
Antin Williams <williamsantin43@gmail.com>
2021-05-11 19:28:24 +0200

    Finished up CollectionApp code

--- src/main/java/za/ac/cput/CollectionApp.java
+++ src/main/java/za/ac/cput/CollectionApp.java
@@ -1,4 +1,16 @@
 package za.ac.cput;
 
+import java.util.Collection;
+import java.util.Stack;
+
 public class CollectionApp {
+    public static void main(String[] args) {
+        Collection<String> collection = new Stack();
+        collection.add("Harry Potter");
+        collection.add("Magicians");
+        collection.add("Game of Thrones");
+        collection.add("Twilight");
+        collection.remove("Twilight");
+        System.out.println("Does the stack contain Magicians? " + collection.contains("Magicians"));
+    }
 }
