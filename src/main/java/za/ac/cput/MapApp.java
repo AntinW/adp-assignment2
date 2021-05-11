9e471ba251e6ab8814b00d9e7f78474784aba674
Antin Williams <williamsantin43@gmail.com>
2021-05-11 16:57:24 +0200

    Finished up MapApp code

--- src/main/java/za/ac/cput/MapApp.java
+++ src/main/java/za/ac/cput/MapApp.java
@@ -1,13 +1,22 @@
 package za.ac.cput;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Hello world!
  *
  */
 public class MapApp
 {
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Map map = new HashMap();
+        map.put(1, "Antin Williams");
+        map.put(2, "John Doe");
+        System.out.println(map);
+        System.out.println(map.get(1));
+        map.remove(1);
     }
 }
