c4e3436cbc2505ce094a311247b7c120daeb55d8
Antin Williams <williamsantin43@gmail.com>
2021-05-11 19:24:11 +0200

    Finished up CollectionAppTest code

--- src/test/java/za/ac/cput/CollectionAppTest.java
+++ src/test/java/za/ac/cput/CollectionAppTest.java
@@ -1,12 +1,34 @@
 package za.ac.cput;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+import java.util.Stack;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CollectionAppTest {
-
+    Collection<String> collection;
     @BeforeEach
     void setUp() {
+        collection = new Stack();
+        collection.add("Harry Potter");
+        collection.add("Game of Thrones");
+        collection.add("Magicians");
+    }
+    @Test
+    public void testCollectionAdd(){
+        collection.add("Twilight");
+        assertTrue(collection.contains("Twilight"));
+    }
+    @Test
+    public void testCollectionRemove(){
+        collection.remove("Harry Potter");
+        assertFalse(collection.contains("Harry Potter"));
+    }
+    @Test
+    public void testCollectionFind(){
+        assertTrue(collection.contains("Magicians"));
     }
 }
